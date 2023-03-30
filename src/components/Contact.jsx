c162a5eb837882cfc644e4dafedde098a962d4bb
MaximProger <maksimkoshman090@gmail.com>
2023-03-30 18:22:26 +0300

    finall

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,49 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_ku3ysl4",
+        "template_ouiukzq",
+        {
+          from_name: form.name,
+          to_name: "Maksim",
+          from_email: form.email,
+          to_email: "malsimkoshman090@gmail.com",
+          message: form.message,
+        },
+        "gglg6V4Gmym37g7pm"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+
+          console.error(error);
+
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
